755bd74f0b4ae9b7e325d6ff3e5b9fcc17fb8a68
Sacoren65 <148143373+Sacoren65@users.noreply.github.com>
2024-12-05 11:32:33 -0500

    Working Level 1

--- PlayLevel.py
+++ PlayLevel.py
@@ -6,16 +6,17 @@ from TerrainSprite import TerrainSprite
 
 #Pygame.init
 pygame.init()
-#This is our main loop where we will do EVERYTHING related to actually playing the game.
 
 #This implementation should be changed depending on settings.
 running = True
 
+Background = pygame.image.load(os.path.join(r"C:\Users\sacor\source\repos\FadiJ98\2D-Game\2D Game Images\Level_Tiles_Sets\Level_1","TileSet3SC.png"))
+#Background.load(os.path.join(r"C:\Users\sacor\source\repos\FadiJ98\2D-Game\2D Game Images\Level_Tiles_Sets\Level_1","Tile Set 3.png"))
 #Main Function
 def GameLoop(Map,screen): 
     #Main Loop
     while running:
-        screen.fill((0,0,0)) #Fill the screen with black.
+        screen.blit(Background,(0,0)) #Fill the screen with background image.
 
         #Properly draw the Map using a 2d array of TerrainSprites given by LevelNode upon running LevelNode.StartLevel
         for i in range(len(Map)):
