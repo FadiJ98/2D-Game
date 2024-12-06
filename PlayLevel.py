02189349f068933da0b8bbda3f4bbe099c117830
Jeremy Matzinger <113539011+InfamousJam@users.noreply.github.com>
2024-12-06 00:50:23 +0000

    Update PlayLevel.py

--- PlayLevel.py
+++ PlayLevel.py
@@ -1,9 +1,33 @@
 import pygame
 import sys
 from Tiny_Dude_Hero import Hero
-
+import os
+from TerrainSprite import TerrainSprite
+from entities import Enemy
+from utility import *
 pygame.init()
 
+class Game:
+    def __init__(self, hero):
+
+        self.assets = {
+            # 'particle/particle' : Animation(load_images('particles/particle'), image_dur=6, loop=False),
+            'enemy/idle' : Animation(load_images('idle'), 6),
+            'enemy/run' : Animation(load_images('walk'), 4)
+        }
+        #enemy creation
+        enemy_size = (8, 15) # change this to the enemy size
+        pos_1 = (0, 0)
+         # change this to the projectile object the player shoots
+        # pos_2 = (0, 0)
+        # pos_3 = (0, 0)
+        self.enemies = []
+        self.enemies.append(Enemy(self,pos_1, enemy_size))
+        # self.enemies.append(Enemy(self,pos_2, enemy_size))
+        # self.enemies.append(Enemy(self,pos_3, enemy_size))
+
+        self.particles = []
+        self.sparks = []
 running = True
 
 # Load background
@@ -13,22 +37,12 @@ Background = pygame.image.load('2D Game Images/Level_Tiles_Sets/Level_1/TileSet3
 # Define a spawn point for the hero
 SPAWN_POINT = (800, 900)  # Example spawn point coordinates (x=100, y=500)
 
-terrainDict = {}
-
-
-
 # Main Function
 def GameLoop(Map, screen):
     global running
     hero = Hero(*SPAWN_POINT)
     clock = pygame.time.Clock()
-
-    #Homeless Element That Should Be somewhere else but isn't because I'm a big dum so it's here for ease of access. From Sam.
-    for i in range(len(Map)):
-        for j in range(len(Map[i])):
-            terrainDict[f"{i},{j}"] = Map[i][j].rect
-
-    print(terrainDict)
+    game = Game(hero)
     while running:
         delta_time = clock.tick(60) / 1000
 
@@ -48,9 +62,6 @@ def GameLoop(Map, screen):
                 if event.key == pygame.K_e:
                     hero.use_ability()
 
-        hero.rect.collidedict()
-
-
         # Clear the screen with the background image
         screen.blit(Background, (0, 0))
 
@@ -60,9 +71,25 @@ def GameLoop(Map, screen):
             for j in range(len(Map[i])):
                 Map[i][j].Draw(screen)
 
+              
+        rock = hero.rocks
+        #draw the enemies and check if the player killed them
+        for enemy in game.enemies.copy():
+            kill = game.enemies.update(screen, rock)
+            game.enemies.render(screen, offset = (0,0))
+            if kill:
+                game.enemies.remove(enemy)
+            hurt = game.enemies.check_hurt(hero)
+            if hurt:
+                hero.take_damage(10)
         # Update and draw the hero
         hero.update(delta_time)
         hero.draw(screen)
 
         # Update the display
         pygame.display.update()
+
+
+
+                
+
